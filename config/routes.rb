6ce0d799c8a3bcbe65d1c35e143b9c5a19dfddf4
may arden <mayard3n@gmail.com>
2022-02-23 17:17:43 -0800

    draws custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :artifacts
-  resources :categories
+  root 'categories#index'
+  
+  resources :categories do 
+    resources :artifacts, only: [:index]
+  end 
+    
+  resources :artifacts, only: [:create, :show]
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
